1d9bb9c0aae74b9df3e4d09e43e8ea15dff3c2e5
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-10-29 19:48:44 -0300

    alter

--- srcs/requirements/mariadb/conf/start_mariadb.sh
+++ srcs/requirements/mariadb/conf/start_mariadb.sh
@@ -6,25 +6,20 @@ while ! rc-service mariadb status | grep -q "is running"; do
     sleep 1
 done
 
-# Check if the MariaDB service started successfully
-if  systemctl is-active --quiet mysqld; then
-	if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
-		echo "\e[0;91m" " [ Database already exists ] " "\e[0m"
-	else
-		# Create Database
-		mysqld -u $MYSQL_ROOT -p $MYSQL_ROOT_PASSWORD \
-			-e "CREATE DATABASE $MYSQL_DATABASE;"
-
-		# Create a user
-		mysqld -u $MYSQL_ROOT -p $MYSQL_ROOT_PASSWORD \
-			-e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY 'MYSQL_PASSWORD';"
-
-		# Give privileges for USER created
-		mysqld -u $MYSQL_ROOT -p $MYSQL_ROOT_PASSWORD \
-			-e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'localhost';"
-
-		mysqld -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-	fi
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+	echo "\e[0;91m" " [ Database already exists ] " "\e[0m"
 else
-	echo "\e[0;91m" " [ MariaDB service failed to start ] " "\e[0m"
+	# Create Database
+	mysqld -u $MYSQL_ROOT -p $MYSQL_ROOT_PASSWORD \
+		-e "CREATE DATABASE $MYSQL_DATABASE;"
+
+	# Create a user
+	mysqld -u $MYSQL_ROOT -p $MYSQL_ROOT_PASSWORD \
+		-e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY 'MYSQL_PASSWORD';"
+
+	# Give privileges for USER created
+	mysqld -u $MYSQL_ROOT -p $MYSQL_ROOT_PASSWORD \
+		-e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'localhost';"
+
+	mysqld -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 fi
