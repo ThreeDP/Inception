d63f9868f22f4f4d77cc52cbc96e835630b64ed3
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-10-29 20:14:42 -0300

    alter

--- srcs/requirements/mariadb/conf/start_mariadb.sh
+++ srcs/requirements/mariadb/conf/start_mariadb.sh
@@ -5,6 +5,8 @@ rc-service mariadb start
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
 	echo "\e[0;91m" " [ Database already exists ] " "\e[0m"
 else
+	mariadb -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
+	
 	# Create Database
 	mysqld -u $MYSQL_ROOT -p $MYSQL_ROOT_PASSWORD \
 		-e "CREATE DATABASE $MYSQL_DATABASE;"
@@ -16,6 +18,7 @@ else
 	# Give privileges for USER created
 	mysqld -u $MYSQL_ROOT -p $MYSQL_ROOT_PASSWORD \
 		-e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'localhost';"
+
 
 	mysqld -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 fi
